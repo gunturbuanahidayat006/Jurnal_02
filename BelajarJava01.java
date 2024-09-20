bac0b51634fa4aca2da9a32f1f9614dc582c0ff9
gunturbuanahidayat006 <gunturdwiputram12@gmail.com>
2024-09-20 07:28:40 +0700

    Add files via upload

---
+++ BelajarJava01.java
@@ -0,0 +1,8 @@
+class BelajarJava01 {
+
+   public static void main(String[] args) {
+      System.out.println("Nama saya Guntur");
+      System.out.println("Nim :607062400029");
+      System.out.println("Kelas D3IF - 48 - 01");
+   }
+}
